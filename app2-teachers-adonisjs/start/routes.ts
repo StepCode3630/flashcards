d0a8b01f3421f73701beb62ed0db6b6fca2254f8
Stepan <pl77sbr@eduvaud.ch>
2026-01-21 14:39:26 +0100

    feat(step4): Terminé
    [35] [Done]

---
+++ app2-teachers-adonisjs/start/routes.ts
@@ -0,0 +1,11 @@
+/*
+|--------------------------------------------------------------------------
+| Le fichier des routes
+|--------------------------------------------------------------------------
+|
+| Le fichier des routes a pour but de définir toutes les routes HTTP.
+|
+*/
+import TeachersController from '#controllers/teachers_controller'
+import router from '@adonisjs/core/services/router'
+router.get('/', [TeachersController, 'index']).as('home')
