0fe1ec3f5dae4560f3678bf4ca7e9dee50120860
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-02-11 14:15:38 +0100

    feat(model): Création des modèles
    [5][Done]

---
+++ src/app/models/card.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Card extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare question: string
+
+  @column()
+  declare answer: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
