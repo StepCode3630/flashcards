49e1c94f135f562cc6131e67de5d23987984714b
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-02-11 14:09:29 +0100

    feat(migration): Création des migrations
    [15] [Done]

---
+++ src/database/migrations/1770814980063_create_cards_table.ts
@@ -0,0 +1,29 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'cards'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('question').notNullable()
+      table.text('answer').notNullable()
+
+      table.timestamp('created_at') //Ces 2 lignes sont optionnels
+      table.timestamp('updated_at')
+
+      table
+        .integer('decks_id')
+        .unsigned()
+        .references('id')
+        .inTable(`decks`)
+        .onDelete(`CASCADE`)
+        .onUpdate(`CASCADE`)
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
