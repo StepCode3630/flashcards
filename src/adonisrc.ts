daf0ba1599c58af72ab91acddc14779280836246
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-02-25 15:11:06 +0100

    feat(create decks): Ajout de la langue fr
    [25] [WIP]

--- src/adonisrc.ts
+++ src/adonisrc.ts
@@ -49,7 +49,8 @@ export default defineConfig({
     () => import('@adonisjs/vite/vite_provider'),
     () => import('@adonisjs/shield/shield_provider'),
     () => import('@adonisjs/static/static_provider'),
-    () => import('@adonisjs/lucid/database_provider')
+    () => import('@adonisjs/lucid/database_provider'),
+    () => import('@adonisjs/i18n/i18n_provider')
   ],
 
   /*
@@ -96,6 +97,10 @@ export default defineConfig({
       pattern: 'public/**',
       reloadServer: false,
     },
+    {
+      pattern: 'resources/lang/**/*.{json,yaml,yml}',
+      reloadServer: false,
+    }
   ],
 
   assetsBundler: false,
